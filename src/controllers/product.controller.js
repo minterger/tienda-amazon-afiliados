0858d7cbc0fb31de234b70e91d6fa571b4ce6f49
minterger <minterger@gmail.com>
2021-01-27 01:32:27 -0300

    update

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -6,4 +6,16 @@ productCtrl.newProduct = (req, res) => {
     res.render('products/new-product')
 }
 
+productCtrl.createNewProduct = (req, res) => {
+    res.send('creando nuevo producto')
+}
+
+productCtrl.editProduct = (req, res) => {
+    res.render('products/edit-product')
+}
+
+productCtrl.endEditProduct = (req, res) => {
+    res.send('producto editado')
+}
+
 module.exports = productCtrl
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,7 +1,18 @@
 const { Router } = require('express')
-const {newProduct} = require('../controllers/product.controller')
+const {
+    newProduct,
+    createNewProduct,
+    editProduct,
+    endEditProduct
+} = require('../controllers/product.controller')
 const router = Router()
 
 router.get('/new-product', newProduct)
+
+router.post('/new-product', createNewProduct)
+
+router.get('/edit-product', editProduct)
+
+router.post('/edit-product', endEditProduct)
 
 module.exports = router
